3f23071d462b7dc84d4ac34182088d78490bc3c4
Ivan <118538755+ivanaraujo12@users.noreply.github.com>
2023-09-14 21:19:02 -0300

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    src: " URL do video",
+    title: "video 1",
+  },
+  {
+    src: "URL do video",
+    title: "video 2",
+  },
+  {
+    src: "URL do video",
+    title: "video 3",
+  },
+  {
+    src: "URL do video",
+    title: "video 4",
+  },
+  {
+    src: "URL do video",
+    title: "video 5",
+  },
+  {
+    src: "URL do video",
+    title: "video 6",
+  },
+  {
+    src: "URL do video",
+    title: "video 7",
+  },
+];
